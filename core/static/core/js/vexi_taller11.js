d9b41936c34f83fa7774bd86b7961dd247027189
juanmoguchy <juanp.mogu@gmail.com>
2021-06-11 20:28:21 -0500

    vexi fixes 2/2

--- core/static/core/js/vexi_taller11.js
+++ core/static/core/js/vexi_taller11.js
@@ -4,7 +4,7 @@ $('document').ready(function() {
     quote: 'NO gastes más de lo que puedes pagar mensualmente: lo ideal es que tu TDC tenga una deuda de un 30% de tu sueldo',
  
   }, {
-    quote: 'NO uses el efectivo: Si se te antojaron unos taquitos en la calle o comprar en el mercadito, lo mejor será que no uses tu tarjeta para evitar las comisiones Cuidado con los meses sin intereses: aunque uno de los mejores consejos que te podemos dar es busca las promociones, trata de no abusar de ellas, aunque podría parecer pagos pequeños, si abusas de ellos podría terminar pagando de más.',
+    quote: 'NO uses el efectivo: Si se te antojaron unos taquitos en la calle o comprar en el mercadito, lo mejor será que no uses tu tarjeta para evitar las comisiones.',
 
   }, {
     quote: 'Trata de evitar comprar a plazos: Si vas a comprar algo trata de que puedas pagarlo en su totalidad al siguiente corte de tu tarjeta, así evitarás intereses. La única manera en la que te recomendamos comprar algo a plazos es en caso de que tu compra tenga una vida superior a lo que durará tu deuda.',
