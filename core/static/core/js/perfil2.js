ece0f0a5d0503d644587c68daceebab8daa76a51
Michell <michell.ra47@gmail.com>
2021-08-10 18:20:45 -0500

    efecto menu2 listo

--- core/static/core/js/perfil2.js
+++ core/static/core/js/perfil2.js
@@ -2,17 +2,17 @@
 /*----------------------------  Editar nickname ---------------------------- */ 
 function edit_nickname() {
 
-    alert("perfil2");
+    // alert("perfil2");
     var nickname = document.querySelector("#perfil2_presentacion_username_2");
-    console.log(" Presionaste el boton de editar nick name. :) ");
+    // console.log(" Presionaste el boton de editar nick name. :) ");
     nickname.style.display = "block";
 
 }
 function close_edit_nickname(){
 
-    alert("Cerrar edit nickname");
+    // alert("Cerrar edit nickname");
     var nickname1 = document.querySelector("#perfil2_presentacion_username_2");
-    console.log("Cerraste el editor de presentacion.");
+    // console.log("Cerraste el editor de presentacion.");
     nickname1.style.display = "none";
 }
 /*----------------------------  Editar nickname ---------------------------- */ 
@@ -21,14 +21,14 @@ function close_edit_nickname(){
 
 function edit_descripcion() {
 
-    alert("editar descripcion");
+    // alert("editar descripcion");
     var nickname = document.querySelector("#perfil2_presentacion_descripcion_edit");
-    console.log(" Presionaste el boton de editar descripcion :) ");
+    // console.log(" Presionaste el boton de editar descripcion :) ");
     nickname.style.display = "block";
     
 }
 function close_edit_descripcion(){
-    alert("cerrar edit descripcion");
+    // alert("cerrar edit descripcion");
     var descripcion = document.querySelector("#perfil2_presentacion_descripcion_edit");
     descripcion.style.display = "none";
 }
