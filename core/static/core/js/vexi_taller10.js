f2a1ff8d89f5edc9ff5914d1e77ae34270e3e31a
juanmoguchy <juanp.mogu@gmail.com>
2021-09-01 16:52:53 -0500

    fixes varios version final

--- core/static/core/js/vexi_taller10.js
+++ core/static/core/js/vexi_taller10.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   
-  var stack1 = $('#vexi_taller_10_stack1');
+  // // var stack1 = $('#vexi_taller_10_stack1');
   
-  stack1.removeClass('vexi_taller_10_start');
+  // stack1.removeClass('vexi_taller_10_start');
   
-  stack1.hammer().on('tap', function(event) {
-    stack1.addClass('hover');
-    event.stopPropagation();
-  });
+  // stack1.hammer().on('tap', function(event) {
+  //   stack1.addClass('hover');
+  //   event.stopPropagation();
+  // });
   
   
-  $(document).hammer().on('tap', function(event) {
-    stack1.removeClass('hover');
-    $('.vexi_taller_10_card').removeClass('hover');
-  });
+  // $(document).hammer().on('tap', function(event) {
+  //   stack1.removeClass('hover');
+  //   $('.vexi_taller_10_card').removeClass('hover');
+  // });
   
 });
